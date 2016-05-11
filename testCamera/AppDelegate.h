d124c23a59d48038f14f2edeafb227734ab69554
panhf <mac@macdemacbook-pro.local>
2016-05-11 15:17:44 +0800

    Initial Commit

---
+++ testCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testCamera
+//
+//  Created by mac on 16/5/11.
+//  Copyright © 2016年 com.chinaums. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testCamera
+//
+//  Created by mac on 16/5/11.
+//  Copyright © 2016年 com.chinaums. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
